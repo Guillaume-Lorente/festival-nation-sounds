19a3ec39de846c66e09f4aef3a76393058c360f7
Guillaume Lorente <guillaumelorente@orange.fr>
2025-06-04 19:23:23 +0200

    ...

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/usersController");
 const verifyToken = require("../middlewares/authMiddleware");
-const validatePasswordStrength = require("../middlewares/validatePasswordStrength");
+const validatePasswordStrength = require("../middlewares/validatePasswordStrenght");
 
 // Inscription
 router.post("/register", validatePasswordStrength, usersController.register);
