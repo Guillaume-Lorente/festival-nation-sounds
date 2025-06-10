f2152527d66cd8e35411e441b1d57dc51542d17d
Guillaume Lorente <guillaumelorente@orange.fr>
2025-06-11 00:12:24 +0200

    ..

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,20 +1,18 @@
 const { Pool } = require("pg");
 
-// Crée une instance de Pool (connexion à PostgreSQL)
-const pool = new Pool(
-  process.env.DATABASE_URL
-    ? {
-        connectionString: process.env.DATABASE_URL,
-        ssl: { rejectUnauthorized: false },
-      }
-    : {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-      }
-);
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    })
+  : new Pool({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
 
-// Exporte la connexion pour l’utiliser dans les modèles
 module.exports = pool;
