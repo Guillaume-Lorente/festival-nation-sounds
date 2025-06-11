eca754ee8b3ca13e72f52dc9330367d1533c2c38
Guillaume Lorente <guillaumelorente@orange.fr>
2025-06-11 12:05:18 +0200

    Changement visuel page account

--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -34,7 +34,7 @@ try {
   return (
   <div className="p-4 max-w-2xl mx-auto">
     <div className="bg-white rounded shadow p-6 mb-6">
-      <h2 className="text-2xl font-bold mb-2">Bienvenue {user?.username} 👋</h2>
+      <h2 className="text-2xl font-bold text-blue-600 mb-2">Bienvenue {user?.username} 👋</h2>
       <p className="text-gray-600">Voici vos artistes favoris :</p>
     </div>
 
@@ -52,15 +52,15 @@ try {
       {favorites.map((artist) => (
         <li
           key={artist.id}
-          className="flex justify-between items-center border rounded p-4 shadow flex-col sm:flex-row sm:items-start sm:space-x-4"
+          className="flex justify-between items-center border rounded p-4 shadow flex-col sm:flex-row sm:items-start sm:space-x-4 bg-white"
         >
           <div className="flex-1 text-left">
-            <p className="text-lg font-semibold">{artist.name}</p>
+            <p className="text-lg font-bold text-blue-600">{artist.name}</p>
             {artist.genre && (
               <p className="text-sm text-gray-600">Genre : {artist.genre}</p>
             )}
             {artist.date && artist.stage && (
-  <p className="text-sm text-blue-600">
+  <p className="text-md text-blue-600">
     📅 {new Date(artist.date).toLocaleDateString()} – 🕒 {new Date(artist.date).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })} – 🎪 {artist.stage}
   </p>
 )}
@@ -71,16 +71,16 @@ try {
               to={`/artist/${artist.id}`}
               target="_blank"
               rel="noopener noreferrer"
-              className="text-sm bg-yellow-400 text-black px-3 py-1 rounded hover:bg-yellow-300"
+              className="px-3 py-1 bg-yellow-300 text-blue-700 rounded hover:bg-yellow-400"
             >
-              Voir la fiche
+            ✏️ Voir la fiche
             </Link>
 
             <button
               onClick={() => removeFavorite(artist.id)}
-              className="text-sm bg-gray-200 text-red-600 px-3 py-1 rounded hover:bg-gray-300"
+              className="px-3 py-1 bg-red-600 text-white rounded hover:bg-red-700"
             >
-              Retirer
+            🗑️ Retirer
             </button>
           </div>
         </li>
